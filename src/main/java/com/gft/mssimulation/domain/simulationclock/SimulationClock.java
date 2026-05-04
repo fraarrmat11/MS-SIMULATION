6f5d8cad0155aade889ada02910a22d8ef1fccaf
miranda9932 <albertmirandabayes@gmail.com>
2026-05-04 17:37:54 +0200

    feat: create and implmenet simulationclock

---
+++ src/main/java/com/gft/mssimulation/domain/simulationclock/SimulationClock.java
@@ -0,0 +1,37 @@
+package com.gft.mssimulation.domain.simulationclock;
+
+import java.util.Objects;
+
+public final class SimulationClock {
+
+    private SimulationDay currentDay;
+
+    private SimulationClock(SimulationDay currentDay) {
+        this.currentDay = Objects.requireNonNull(currentDay, "currentDay cannot be null");
+    }
+
+    public static SimulationClock initial() {
+        return new SimulationClock(SimulationDay.zero());
+    }
+
+    public static SimulationClock of(SimulationDay currentDay) {
+        return new SimulationClock(currentDay);
+    }
+
+    public TimeAdvancedEvent advanceDay(int daysToAdvance) {
+        SimulationDay previousDay = currentDay;
+        SimulationDay advancedDay = currentDay.advanceBy(daysToAdvance);
+
+        currentDay = advancedDay;
+
+        return TimeAdvancedEvent.occurred(
+                previousDay.dayNumber(),
+                advancedDay.dayNumber(),
+                daysToAdvance
+        );
+    }
+
+    public SimulationDay getCurrentDay() {
+        return currentDay;
+    }
+}
