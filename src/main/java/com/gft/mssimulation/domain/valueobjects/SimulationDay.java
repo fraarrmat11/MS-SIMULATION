3bd8ccb8b8de4ad3f5738520b0697f214cc26bfe
fran <fraarrmat11@gmail.com>
2026-05-04 12:20:40 +0200

    feature: added value object SimulationDay

---
+++ src/main/java/com/gft/mssimulation/domain/valueobjects/SimulationDay.java
@@ -0,0 +1,12 @@
+package com.gft.mssimulation.domain.valueobjects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class SimulationDay {
+    int value;
+}
