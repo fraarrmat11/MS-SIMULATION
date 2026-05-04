38402a2f3b7cb824e9a73bff900a4387f696e7c2
miranda9932 <albertmirandabayes@gmail.com>
2026-05-04 11:43:47 +0200

    feat: added coverage 100%

--- src/main/java/com/gft/mssimulation/domain/valueobjects/Location.java
+++ src/main/java/com/gft/mssimulation/domain/valueobjects/Location.java
@@ -1,5 +1,13 @@
 package com.gft.mssimulation.domain.valueobjects;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Location {
     private int x;
     private int y;
