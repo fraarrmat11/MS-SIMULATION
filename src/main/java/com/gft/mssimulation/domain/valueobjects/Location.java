e7ef4589be6e30b2e9f4916155204687d3ee24b1
fran <fraarrmat11@gmail.com>
2026-05-04 08:44:35 +0200

    feature: added value objects

---
+++ src/main/java/com/gft/mssimulation/domain/valueobjects/Location.java
@@ -0,0 +1,6 @@
+package com.gft.mssimulation.domain.valueobjects;
+
+public class Location {
+    private int x;
+    private int y;
+}
---
+++ src/main/java/com/gft/mssimulation/domain/valueobjects/TruckPosition.java
@@ -0,0 +1,8 @@
+package com.gft.mssimulation.domain.valueobjects;
+
+import java.util.UUID;
+
+public class TruckPosition {
+    UUID truckId;
+    Location location;
+}
