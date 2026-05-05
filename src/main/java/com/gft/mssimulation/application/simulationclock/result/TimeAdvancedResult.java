6560808f6ba4a28b65701835aac74b174198dbdd
miranda9932 <albertmirandabayes@gmail.com>
2026-05-05 11:19:55 +0200

    feat: added time layer

---
+++ src/main/java/com/gft/mssimulation/application/simulationclock/result/TimeAdvancedResult.java
@@ -0,0 +1,25 @@
+package com.gft.mssimulation.application.simulationclock.result;
+
+import com.gft.mssimulation.domain.simulationclock.TimeAdvancedEvent;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record TimeAdvancedResult(
+        UUID eventId,
+        int previousDay,
+        int currentDay,
+        int daysAdvanced,
+        Instant occurredAt
+) {
+
+    public static TimeAdvancedResult from(TimeAdvancedEvent event) {
+        return new TimeAdvancedResult(
+                event.eventId(),
+                event.previousDay(),
+                event.currentDay(),
+                event.daysAdvanced(),
+                event.occurredAt()
+        );
+    }
+}
