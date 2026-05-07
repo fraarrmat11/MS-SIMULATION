fff6f54c081e8f28e80f36e3f28535c308be7c7d
fran <fraarrmat11@gmail.com>
2026-05-07 08:57:03 +0200

    feature: add map api and initializer

--- src/main/java/com/gft/mssimulation/application/mapstate/MapStateInitializer.java
+++ src/main/java/com/gft/mssimulation/application/mapstate/MapStateInitializer.java
@@ -2,10 +2,12 @@ package com.gft.mssimulation.application.mapstate;
 
 import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
+@Profile("!test")
 public class MapStateInitializer {
     private final LoadMapStateUseCase loadMapStateUseCase;
     private final MapStateHolder holder;
