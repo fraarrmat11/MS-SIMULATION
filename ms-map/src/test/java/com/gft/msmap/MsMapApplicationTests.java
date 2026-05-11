9578c09e09f54c160359c4ed2f032a7e9c9ae70f
fran <fraarrmat11@gmail.com>
2026-05-11 13:27:23 +0200

    refactor: change package structure

--- ms-map/src/test/java/com/gft/msmap/MsMapApplicationTests.java
+++ ms-map/src/test/java/com/gft/msmap/MsMapApplicationTests.java
@@ -1,8 +1,12 @@
 package com.gft.msmap;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -10,6 +14,17 @@ class MsMapApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void mainStartsSpringApplication() {
+        String[] args = {"--spring.main.web-application-type=none"};
+
+        try (MockedStatic<SpringApplication> springApplication = mockStatic(SpringApplication.class)) {
+            MsMapApplication.main(args);
+
+            springApplication.verify(() -> SpringApplication.run(MsMapApplication.class, args));
+        }
     }
 
 }
