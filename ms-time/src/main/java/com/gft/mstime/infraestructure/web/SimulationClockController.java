fbc8f20b36a353e237f78dc189ef78cb172c9eb3
fran <fraarrmat11@gmail.com>
2026-05-11 15:06:59 +0200

    refactor: quit requestbody in post tick

--- ms-time/src/main/java/com/gft/mstime/infraestructure/web/SimulationClockController.java
+++ ms-time/src/main/java/com/gft/mstime/infraestructure/web/SimulationClockController.java
@@ -3,13 +3,14 @@ package com.gft.mstime.infraestructure.web;
 import com.gft.mstime.application.usecase.AdvanceTimeUseCase;
 import com.gft.mstime.application.command.AdvanceTimeCommand;
 import com.gft.mstime.application.result.TimeAdvancedResult;
-import com.gft.mstime.infraestructure.web.request.AdvanceTimeRequest;
+import com.gft.mstime.domain.SimulationDay;
 import com.gft.mstime.infraestructure.web.response.TimeAdvancedResponse;
 import jakarta.validation.Valid;
+import lombok.NonNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
@@ -22,10 +23,12 @@ public class SimulationClockController {
         this.advanceTimeUseCase = Objects.requireNonNull(advanceTimeUseCase, "advanceTimeUseCase cannot be null");
     }
 
-    @PostMapping("/tick")
-    public ResponseEntity<TimeAdvancedResponse> advanceTime(@Valid @RequestBody AdvanceTimeRequest request) {
-        TimeAdvancedResult result = advanceTimeUseCase.advanceTime(new AdvanceTimeCommand(request.days()));
-
+    @PostMapping("/tick/{days}")
+    public ResponseEntity<TimeAdvancedResponse> advanceTime(@Valid @PathVariable int days) {
+        if(days < 1){
+            return ResponseEntity.status(HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE).build();
+        }
+        TimeAdvancedResult result = advanceTimeUseCase.advanceTime(new AdvanceTimeCommand(days));
         return ResponseEntity.ok(TimeAdvancedResponse.from(result));
     }
 }
