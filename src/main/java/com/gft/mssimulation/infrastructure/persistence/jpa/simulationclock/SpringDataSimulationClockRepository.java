0ad1b2483ec6d929b2f2e44c7cf692bb7ab9b774
miranda9932 <albertmirandabayes@gmail.com>
2026-05-11 13:13:13 +0200

    feature: add agents md

--- src/main/java/com/gft/mssimulation/infrastructure/persistence/jpa/simulationclock/SpringDataSimulationClockRepository.java
+++ src/main/java/com/gft/mssimulation/infrastructure/persistence/jpa/simulationclock/SpringDataSimulationClockRepository.java
@@ -1,6 +1,8 @@
 package com.gft.mssimulation.infrastructure.persistence.jpa.simulationclock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SpringDataSimulationClockRepository extends JpaRepository<SimulationClockEntity, Long> {
 }
