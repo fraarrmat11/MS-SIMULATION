e87d991e32cae60cb5b4254e14e4412a7e1db886
mion <913mario@gmail.com>
2026-05-14 16:31:00 +0200

    feature: add logging to ms-map use cases

--- ms-map/src/main/java/com/gft/msmap/application/usecase/RegisterTruckUseCase.java
+++ ms-map/src/main/java/com/gft/msmap/application/usecase/RegisterTruckUseCase.java
@@ -4,12 +4,13 @@ import com.gft.msmap.application.port.out.TruckPositionPort;
 import com.gft.msmap.application.service.MapStateHolder;
 import com.gft.msmap.domain.Location;
 import com.gft.msmap.domain.TruckPosition;
-import com.gft.msmap.domain.exceptions.TruckAlreadyRegisteredException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class RegisterTruckUseCase {
@@ -17,8 +18,10 @@ public class RegisterTruckUseCase {
     private final MapStateHolder holder;
     private final TruckPositionPort truckPositionPort;
 
-    public void execute(UUID truckId, Location location){
+    public void execute(UUID truckId, Location location) {
+        log.info("Registering truck: truckId={}", truckId);
         holder.get().registerTruck(truckId, location);
-        truckPositionPort.save(new TruckPosition(truckId,location));
+        truckPositionPort.save(new TruckPosition(truckId, location));
+        log.info("Truck registered successfully: truckId={}", truckId);
     }
 }
--- ms-map/src/main/java/com/gft/msmap/application/usecase/UpdateTruckPositionUseCase.java
+++ ms-map/src/main/java/com/gft/msmap/application/usecase/UpdateTruckPositionUseCase.java
@@ -5,10 +5,12 @@ import com.gft.msmap.application.service.MapStateHolder;
 import com.gft.msmap.domain.Location;
 import com.gft.msmap.domain.TruckPosition;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class UpdateTruckPositionUseCase {
@@ -17,7 +19,9 @@ public class UpdateTruckPositionUseCase {
     private final TruckPositionPort truckPositionPort;
 
     public void execute(UUID truckId, Location location) {
+        log.info("Updating truck position: truckId={}", truckId);
         holder.get().updateTruckPosition(truckId, location);
         truckPositionPort.save(new TruckPosition(truckId, location));
+        log.info("Truck position updated successfully: truckId={}", truckId);
     }
 }
