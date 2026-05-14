e40b4635eb84fdcc5063de008ef2888a3d3c9697
mion <913mario@gmail.com>
2026-05-14 16:03:00 +0200

    feature: add logging to ms-map RabbitMQ listeners

--- ms-map/src/main/java/com/gft/msmap/infraestructure/messaging/rabbitmq/listener/TruckPositionUpdatedListener.java
+++ ms-map/src/main/java/com/gft/msmap/infraestructure/messaging/rabbitmq/listener/TruckPositionUpdatedListener.java
@@ -4,9 +4,11 @@ import com.gft.msmap.application.usecase.UpdateTruckPositionUseCase;
 import com.gft.msmap.infraestructure.config.RabbitMQConfig;
 import com.gft.msmap.infraestructure.messaging.rabbitmq.TruckPositionUpdatedEvent;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class TruckPositionUpdatedListener {
@@ -15,13 +17,12 @@ public class TruckPositionUpdatedListener {
 
     @RabbitListener(queues = RabbitMQConfig.TRUCK_POSITION_UPDATED_QUEUE)
     public void onEvent(TruckPositionUpdatedEvent event) {
-        try{
-            useCase.execute(
-                    event.getTruckId(),
-                    event.getLocation()
-            );
-        } catch (IllegalArgumentException e){
-            System.out.println(e.getMessage() + event.getTruckId());
+        log.debug("Received TruckPositionUpdated event: truckId={}", event.getTruckId());
+        try {
+            useCase.execute(event.getTruckId(), event.getLocation());
+            log.debug("TruckPositionUpdated processed successfully: truckId={}", event.getTruckId());
+        } catch (IllegalArgumentException e) {
+            log.error("Failed to update position for truckId={}: {}", event.getTruckId(), e.getMessage());
         }
     }
 }
