4004ce67091cd2666df69ff1eea7ada53ae3138b
fran <fraarrmat11@gmail.com>
2026-05-18 15:18:27 +0200

    feature: add delete truck event class and listener

---
+++ src/main/java/com/gft/simulation/map/internal/infrastructure/messaging/rabbitmq/listener/TruckDeletedListener.java
@@ -0,0 +1,29 @@
+package com.gft.simulation.map.internal.infrastructure.messaging.rabbitmq.listener;
+
+import com.gft.simulation.map.internal.application.usecase.DeleteTruckUseCase;
+import com.gft.simulation.map.internal.domain.exceptions.TruckNotFoundException;
+import com.gft.simulation.map.internal.infrastructure.config.MapRabbitMQConfig;
+import com.gft.simulation.map.internal.infrastructure.messaging.rabbitmq.TruckDeletedEvent;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class TruckDeletedListener {
+
+    private final DeleteTruckUseCase deleteTruckUseCase;
+
+    @RabbitListener(queues = MapRabbitMQConfig.TRUCK_DELETED_QUEUE)
+    public void onEvent(TruckDeletedEvent event){
+        log.debug("Received TruckDeleted event: truckId={}", event.getTruckId());
+        try{
+            deleteTruckUseCase.execute(event.getTruckId());
+            log.debug("TruckDeleted processed successfully: truckId={}", event.getTruckId());
+        }catch (TruckNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
